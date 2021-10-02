e33ead2199331e9de6e62b78f21ff85f01cdcabf
VietKhooi <91773446+VietKhooi@users.noreply.github.com>
2021-10-02 16:22:52 +0700

    Add files via upload

---
+++ khoi.py
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Chào đằng đó, mình là Khôi',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Bạn có thích Viết Khôi không? 🥰',
+    desc: 'Cho mình câu trả lời nha ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Vì yêu bạn <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay đi chơi nha :)).',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://fb.com'
+}
